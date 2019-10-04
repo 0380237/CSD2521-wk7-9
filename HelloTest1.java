5b5183b8183c4f9a4d7b79daeee975b07b230bc8
0380237 <54303347+0380237@users.noreply.github.com>
2019-10-04 05:29:40 -0400

    Changed Comments
    I changed the comment on line 8 and added the updated comment to line 2

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,11 @@
 /*
- * Created on 8/3/18 by AJT
+ * Created on 8/3/18 by AJT, Updated on 10/04/2019 by NLM
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * "This is my first time with Github!"
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
